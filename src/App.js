07608bc8e7c220a6404fb26e5009f02a61b9a4b1
Govamarket <govaway@gmail.com>
2025-04-07 01:35:20 +0100

    initial commmit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./Components/Navigation";
+import Views from "./Components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Views />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Counter.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+
+const Views = () => {
+  const [count, setCount] = useState(0);
+
+  const [autoCount, setAutoCount] = useState(0);
+
+  useEffect(() => {
+    const interVal = setInterval(() => {
+      setAutoCount((prev) => prev + 1);
+    }, 3000);
+
+    return () => clearInterval(interVal);
+  }, []);
+
+  return (
+    <div>
+      <header className="header">
+        <div className="counter">
+          <div onClick={() => setCount(count - 1)}>
+            <h1>-</h1>
+          </div>
+
+          <div>
+            <h1>{count}</h1>
+          </div>
+
+          <div onClick={() => setCount(count + 1)}>
+            <h1>+</h1>
+          </div>
+
+          <div onClick={() => setAutoCount(autoCount + 1)}>
+            <h1>{autoCount}</h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Views;
